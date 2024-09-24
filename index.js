ef3ee9cf8225108c684cbc72d69d0131d0a1ea60
rovenelabanga <rovenelabanga2001@gmail.com>
2024-09-24 18:36:18 +0300

    uses key function aspects such as hoisting, scope chain, closure, ananonymous functions, function declaration, function expression and many more

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective (two ="*"){
+    return function(one= "special"){
+        return `You are ${two}${one}${two}!`
+    }
+}
+wrapAdjective()("a hard worker");
+wrapAdjective("||")("a hard worker");
